74c3ba38be4dc60b0504f6ab834c05705daef413
Nick Peirson <nickpeirson@gmail.com>
2016-10-09 16:56:20 +0100

    Improve titles and add iframe messaging

--- js/app.js
+++ js/app.js
@@ -6,3 +6,49 @@ $('.pricing-tab').click( function() {
 $('.image-selector input').change(function(e) {
   $(e.target).closest('.waxing-treatment').find('img.view').toggle();
 })
+
+function getClientId(uaNumber) {
+  var _ga = window[window.GoogleAnalyticsObject];
+  if (_ga === undefined) {
+    return;
+  }
+  var trackers = _ga.getAll();
+  var i;
+
+  for (i = 0; i < trackers.length; i++) {
+    var _tracker = trackers[i];
+    if (!uaNumber || _tracker.get('trackingId') === uaNumber) {
+      return _tracker.get('clientId');
+    }
+  }
+}
+
+function transmitClientId(evt) {
+  var clientId = getClientId();
+
+  if(clientId) {
+    iframe = $('iframe[src*=\'https://ovatu.com\']')[0].contentWindow;
+    iframe.postMessage('clientId:' + clientId, 'https://ovatu.com');
+  }
+}
+
+function addEvent(el, evt, fn) {
+  if (el.addEventListener) {
+    el.addEventListener(evt, fn);
+  } else if (el.attachEvent) {
+    el.attachEvent('on' + evt, function(evt) {
+      fn.call(el, evt);
+    });
+  } else if (typeof el['on' + evt] === 'undefined' || el['on' + evt] === null) {
+    el['on' + evt] = function(evt) {
+      fn.call(el, evt);
+    };
+  }
+}
+
+
+addEvent(window, 'message', function(message) {
+  if (message.data === 'ovatuLoaded' && message.origin === 'https://ovatu.com') {
+    transmitClientId();
+  }
+});
--- js/pw.js
+++ js/pw.js
@@ -15,7 +15,53 @@ d.parent(".dropdown-menu").length&&(d=d.closest("li.dropdown").addClass("active"
 
 $('.image-selector input').change(function(e) {
   $(e.target).closest('.waxing-treatment').find('img.view').toggle();
-});/*!
+})
+
+function getClientId(uaNumber) {
+  var _ga = window[window.GoogleAnalyticsObject];
+  if (_ga === undefined) {
+    return;
+  }
+  var trackers = _ga.getAll();
+  var i;
+
+  for (i = 0; i < trackers.length; i++) {
+    var _tracker = trackers[i];
+    if (!uaNumber || _tracker.get('trackingId') === uaNumber) {
+      return _tracker.get('clientId');
+    }
+  }
+}
+
+function transmitClientId(evt) {
+  var clientId = getClientId();
+
+  if(clientId) {
+    iframe = $('iframe[src*=\'https://ovatu.com\']')[0].contentWindow;
+    iframe.postMessage('clientId:' + clientId, 'https://ovatu.com');
+  }
+}
+
+function addEvent(el, evt, fn) {
+  if (el.addEventListener) {
+    el.addEventListener(evt, fn);
+  } else if (el.attachEvent) {
+    el.attachEvent('on' + evt, function(evt) {
+      fn.call(el, evt);
+    });
+  } else if (typeof el['on' + evt] === 'undefined' || el['on' + evt] === null) {
+    el['on' + evt] = function(evt) {
+      fn.call(el, evt);
+    };
+  }
+}
+
+
+addEvent(window, 'message', function(message) {
+  if (message.data === 'ovatuLoaded' && message.origin === 'https://ovatu.com') {
+    transmitClientId();
+  }
+});;/*!
  * IE10 viewport hack for Surface/desktop Windows 8 bug
  * Copyright 2014-2015 Twitter, Inc.
  * Licensed under MIT (https://github.com/twbs/bootstrap/blob/master/LICENSE)
